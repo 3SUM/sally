941d8ed688f66a75996bf528b9a357d150eeb6dc
Luis Maya Aranda <mayafluis@gmail.com>
2021-02-10 17:29:33 -0800

    Added discord.Intents

--- bot.py
+++ bot.py
@@ -5,10 +5,15 @@ TOKEN = os.environ.get('TOKEN')
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print('Logged in as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
-client.run(TOKEN)
+
+if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.members = True
+    
+    client = MyClient(intents=intents)
+    client.run(TOKEN)
