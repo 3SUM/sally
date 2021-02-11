bd72c3184a89c18494864c1e4b037f65d2545e21
Luis Maya Aranda <mayafluis@gmail.com>
2021-02-10 20:31:43 -0800

    hope this works

--- bot.py
+++ bot.py
@@ -4,26 +4,22 @@ from discord.ext import commands
 
 TOKEN = os.environ.get('TOKEN')
 
-class Sally:
-    def __init__(self, bot):
-        self.bot = bot
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
+class Sally:
     @bot.event
-    async def on_ready(self):
-        print(f'Logged in as {self.bot.user.name}')
+    async def on_ready():
+        print(f'Logged in as {bot.user.name}')
     
     @bot.command()
-    async def add(self, ctx, left: int, right: int):
+    async def add(ctx, left: int, right: int):
         await ctx.send(left + right)
     
-    def main(self):
-        self.bot.run(TOKEN)
+    def main():
+        bot.run(TOKEN)
 
 
 if __name__ == '__main__':
-    intents = discord.Intents.default()
-    intents.members = True
-    
-    bot = commands.Bot(command_prefix='!', intents=intents)
-    sally = Sally(bot)
-    sally.main()
+    Sally.main()
