8558b4658ff57bf7c2f89ebfdab2defabb5b91cb
Luis Maya Aranda <mayafluis@gmail.com>
2021-02-17 23:00:33 -0800

    updating ticket support

--- bot.py
+++ bot.py
@@ -29,8 +29,30 @@ class Sally:
     async def ticket(ctx, course = "default"):
         if str(course) in Sally.courses:
             guild = ctx.message.guild
-            await guild.create_text_channel(f'ticket-{course}-{ctx.message.author}')
-            await ctx.send(f'Selected course ticket: {course}')
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                ctx.message.author: discord.PermissionOverwrite(read_messages=True)
+            }
+            ticket_create = await guild.create_text_channel(
+                name=(f'ticket-{course}-{ctx.message.author}'),
+                overwrites=overwrites
+            )
+            ticket_embed = discord.Embed(
+                title="Ticket",
+                description=(f'{ctx.message.author.mention}\nPlease be patient. A TA will be with you shortly.'),
+                color=0x15a513
+            )
+            ticket_embed.set_footer(
+                text=(f'Ticket requested by {ctx.message.author}'),
+                icon_url=ctx.message.author.avatar_url
+            )
+            await ticket_create.send(embed=ticket_embed)
+            success_embed = discord.Embed(
+                title="Ticket Creation",
+                description=(f'{ctx.message.author.mention}, your ticket was successfully created: {ticket_create.mention}'),
+                color=0x15a513
+            )
+            await ctx.send(embed=success_embed)
         else:
             await ctx.send(f'`!ticket <course number>`\n\nAccess course number options using `!classes`')
 
