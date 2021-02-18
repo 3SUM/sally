d2eb74daf23c2aaebd98dff4cfe588ddd07d73ed
Luis Maya Aranda <mayafluis@gmail.com>
2021-02-17 23:51:55 -0800

    why is it not working

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ class Sally:
     
     @bot.event
     async def on_member_join(member):
-        print(discord.utils.get(bot.guild.roles))
+        print(member.guild.roles)
     
     @bot.event
     async def on_message(message):
