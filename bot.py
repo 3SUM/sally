7c3b46546deee1af05dc7298427069f0d80aa6c3
Luis Maya Aranda <mayafluis@gmail.com>
2021-03-10 00:35:12 -0800

    Trying to get embeds

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ class Sally:
             if ctx.message.channel.name.find("ticket") > -1:
                 send_to = None
                 messages = await ctx.message.channel.history(limit=999).flatten()
-                print(messages[-1].content)
+                print(messages[-1].embeds)
                 with open("ticket.txt", "w") as file:
                     for message in reversed(messages):
                         file.write(
